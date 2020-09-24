d35ec4956af09f5f7746cb7e98434afb22fcd8bf
musicalcircuits <aslangeb@ncsu.edu>
2020-09-24 14:17:38 -0400

    MainWindow v2

--- mainWindow_app.py
+++ mainWindow_app.py
@@ -1,6 +1,6 @@
 """"
 =============
-Mqin Window
+Main Window
 =============
 """""
 import sys
@@ -8,6 +8,8 @@ from PyQt5 import QtWidgets as qtw
 from PyQt5 import QtGui as qtg
 from PyQt5 import QtCore as qtc
 from PyQt5 import uic
+from transport_app import TransportWidget
+from meas_display_app import MeasurmentDisplayWindow
 # from settingsWindow_form import Ui_SettingsWindow  # Import qtdesigner object
 
 baseUIClass, baseUIWidget = uic.loadUiType('main_window_ui.ui')
@@ -19,9 +21,23 @@ class MyMainWindow(baseUIWidget, baseUIClass):
         """MainWindow constructor"""
         super().__init__()
         self.setupUi(self)
+
         # Main UI code goes here
 
+        """ Adding custom toolbars """
+        transport_toolbar = self.addToolBar('Transport_ToolBar')
+        meas_display_toolbar = self.addToolBar('Measurement_Display_ToolBar')
+
+        """Add Instances of custom widgets"""
+        self.transport = TransportWidget()
+        self.meas_disp_window = MeasurmentDisplayWindow()
+
+        """Add widgets to the toolbars"""
+        meas_display_toolbar.addWidget(self.meas_disp_window)
+        transport_toolbar.addWidget(self.transport)
+
         # End main UI code
+
         self.show()
 
 
---
+++ transport_app.py
@@ -0,0 +1,30 @@
+""""
+=============
+Transport
+=============
+Displays empty window
+"""""
+import sys
+from PyQt5 import QtWidgets as qtw, uic
+from PyQt5 import QtGui as qtg
+from PyQt5 import QtCore as qtc
+
+baseUIClass, baseUIWidget = uic.loadUiType('transport_ui.ui')
+
+
+class TransportWidget(baseUIWidget, baseUIClass):
+
+    def __init__(self):
+        """MainWindow constructor"""
+        super().__init__()
+        self.setupUi(self)
+        # Main UI code goes here
+
+        # End main UI code
+        self.show()
+
+
+if __name__ == '__main__':
+    app = qtw.QApplication(sys.argv)
+    tw = TransportWidget()
+    sys.exit(app.exec())
