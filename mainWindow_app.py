88e336df81ae8b6e20180553933ee360037bf103
Austin <46213786+musicalcircuit@users.noreply.github.com>
2020-09-24 10:29:03 -0400

    Add files via upload
    Main Window v1

---
+++ mainWindow_app.py
@@ -0,0 +1,31 @@
+""""
+=============
+Mqin Window
+=============
+"""""
+import sys
+from PyQt5 import QtWidgets as qtw
+from PyQt5 import QtGui as qtg
+from PyQt5 import QtCore as qtc
+from PyQt5 import uic
+# from settingsWindow_form import Ui_SettingsWindow  # Import qtdesigner object
+
+baseUIClass, baseUIWidget = uic.loadUiType('main_window_ui.ui')
+
+
+class MyMainWindow(baseUIWidget, baseUIClass):
+
+    def __init__(self):
+        """MainWindow constructor"""
+        super().__init__()
+        self.setupUi(self)
+        # Main UI code goes here
+
+        # End main UI code
+        self.show()
+
+
+if __name__ == '__main__':
+    app = qtw.QApplication(sys.argv)
+    mw = MyMainWindow()
+    sys.exit(app.exec())
