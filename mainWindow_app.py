c65838e88a2f870cdef09a7843aebed5115046b6
Tommy <tomhoover1@gmail.com>
2020-09-27 01:05:32 -0400

    Connected current angle signals to LCDNumber
    Connected current_pan and current_tilt signals from meas_ctrl to their respective LCDNumber.display slots, tested and found that they update the LCDNumber displays in the main GUI window, however, only at the end of completing a full sweep, this should be corrected once we get start_mc to not block the main GUI event loop.

--- mainWindow_app.py
+++ mainWindow_app.py
@@ -11,8 +11,8 @@ from PyQt5 import uic
 from transport_app import TransportWidget
 from meas_display_app import MeasurmentDisplayWindow
 import settingsWindow_app
-
-# from settingsWindow_form import Ui_SettingsWindow  # Import qtdesigner object
+import measurement_ctrl as mc
+import json
 
 baseUIClass, baseUIWidget = uic.loadUiType('main_window_ui.ui')
 
@@ -25,6 +25,9 @@ class MyMainWindow(baseUIWidget, baseUIClass):
         self.setupUi(self)
 
         # Main UI code goes here
+
+        """Defining variables"""
+        self.meas_in_progress = False
 
         """ Adding custom toolbars """
         transport_toolbar = self.addToolBar('Transport_ToolBar')
@@ -41,6 +44,11 @@ class MyMainWindow(baseUIWidget, baseUIClass):
         """Connecting settings button to settings window"""
         self.toolBar.actionTriggered.connect(self.show_settings)
 
+        """Connecting transport buttons to meas_ctrl functions"""
+        self.transport.playButton.clicked.connect(self.start_mc)
+        self.transport.pauseButton.clicked.connect(self.pause_mc)
+        self.transport.stopButton.clicked.connect(self.stop_mc)
+
         # End main UI code
 
         self.show()
@@ -48,6 +56,28 @@ class MyMainWindow(baseUIWidget, baseUIClass):
     def show_settings(self):
         self.s = settingsWindow_app.SettingsWindow()
         self.s.show()
+
+    def start_mc(self):
+        if self.meas_in_progress:
+            self.meas_ctrl.run()
+        else:
+            if self.s.settings_empty == False:
+                with open('pivot.json') as file:
+                    dict = json.load(file)
+                self.meas_ctrl = mc.meas_ctrl(dict)
+                self.meas_ctrl.signals.current_pan.connect(self.meas_disp_window.az_lcdNumber.display)
+                self.meas_ctrl.signals.current_tilt.connect(self.meas_disp_window.el_lcdNumber.display)
+                self.meas_ctrl.setup()
+                self.meas_in_progress = True
+                self.meas_ctrl.run()
+
+    def stop_mc(self):
+        self.meas_ctrl.halt()
+        self.meas_in_progress = False
+
+    def pause_mc(self):
+        self.meas_ctrl.halt()
+        self.meas_in_progress = False
 
 
 if __name__ == '__main__':
